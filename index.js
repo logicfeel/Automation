2a413b75e2d1bdf72b1e1f8c0087fdcfedd13182
logicfeel@nate.com <logicfeel@nate.com>
2018-03-02 17:27:03 +0900

    클래스 작업중

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var util            = require('util');
+var EventEmitter    = require('events');
+
+
+function AutoBase() {
+    EventEmitter.call(this);
+
+    this.CONFIG = null;
+    this.PKG = null;
+    this.LOG = null;
+
+}
+util.inherits(AutoBase, EventEmitter);
+
+AutoBase.prototype.init = function() {
+};
+
+AutoBase.prototype.default = function() {
+};
+
+AutoBase.prototype.update = function() {
+};
+
+AutoBase.prototype.preinstall = function() {
+};
+
+AutoBase.prototype.install = function() {
+};
+
+
+//#####################################
+// AutoModule
+function AutoModule() {
+
+}
+util.inherits(AutoModule, AutoBase);
+
+
+//#####################################
+// AutoModuleModel
+function AutoModuleModel() {
+    
+}
+util.inherits(AutoModuleModel, AutoModule);
+
+
+//#####################################
+// 테스트
+var a = new AutoBase();
+
+
+console.log('End');
