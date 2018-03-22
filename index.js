1f14adcfdf04f41cd1e736963d5ff57c27f8aafa
logicfeel@nate.com <logicfeel@nate.com>
2018-03-22 12:23:34 +0900

    index exprot 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 var AutoModModel        = require('./src/AutoModModel');
+var AutoTempalte          = require('r.x.x-auto').AutoTempalte;
 
 module.exports.AutoModModel  = AutoModModel;
+module.exports.AutoTempalte  = AutoTempalte;
