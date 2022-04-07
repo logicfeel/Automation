6478507f836053a99364e1950ee019e5a39a9677
logicfeel <logicfeel@nate.com>
2022-04-07 09:30:34 +0900

    기본템플릿 노출

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var AutoBase            = require('./src/AutoBase');
 var AutoTempalte        = require('./src/AutoTemplate');
 var AutoModule          = require('./src/AutoModule');
+var BaseTemplate        = require('./BaseTemplate');
 
 
 module.exports = {
     AutoBase: AutoBase,
     AutoTempalte: AutoTempalte,
-    AutoModule: AutoModule
+    AutoModule: AutoModule,
+    BaseTemplate: BaseTemplate,
 };
